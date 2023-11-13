f013f81cf511c4ae76eb8157acc62da3eb41d1c5
Tom Dent <tomdent991@hotmail.com>
2023-11-13 13:52:32 +0100

    form and card adding functionality

---
+++ js/form.js
@@ -0,0 +1,66 @@
+const form = document.querySelector('[data-js="form"]');
+const questionInput = document.querySelector('[data-js="question"]');
+const answerInput = document.querySelector('[data-js="answer"]')
+
+const bookmarkButton = document.querySelector('[data-js="bookmarkButton"]');
+const showAnswerButton = document.querySelector('[data-js="show-answer"]');
+const answerReveal = document.querySelector('[data-js="answer"]');
+
+const charactersQuestion = document.querySelector('[data-js="charactersQuestion"]')
+const charactersAnswer = document.querySelector('[data-js="charactersAnswer"]')
+
+questionInput.addEventListener("input", (event) => {
+    const characterLength = event.target.value.length;
+    // console.log(characterLength);
+
+    const charactersRemaining = 150 - characterLength;
+    // console.log(charactersRemaining);
+
+    charactersQuestion.textContent = charactersRemaining;
+})
+
+answerInput.addEventListener("input", (event) => {
+
+    const characterLength = event.target.value.length;
+    // console.log(characterLength);
+
+    const charactersRemaining = 150 - characterLength;
+    // console.log(charactersRemaining);
+
+    charactersAnswer.textContent = charactersRemaining;
+})
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    const formElements = event.target.elements;
+    console.log(formElements);
+  
+    // formElements.question.focus();
+  
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    // console.log(data.question);
+
+    const cardContainer = document.createElement("section");
+
+    document.body.append(cardContainer);
+    cardContainer.classList.add("question-card__container")
+
+    cardContainer.innerHTML = `
+    <button type="button" class="question-card__item question-card__item__bookmark question-card__item__bookmark__unfilled" data-js="bookmarkButton"></button>
+            <h2 class="question-card__item question-card__item__question">${data.question}</h2>
+
+            <button type="button" class="question-card__item question-card__item__show-answer" data-js="show-answer">Show Answer</button>
+
+            <div class="question-card__item question-card__item__answer" data-js="answer" hidden>${data.answer}</div>
+
+            <div class="question-card__item question-card__item__tag-container">
+                <p>#${data.tag}</p>
+            </div>`
+
+    event.target.reset();
+  });
+  
--- js/index.js
+++ js/index.js
@@ -1,17 +1,32 @@
-const bookmark = document.querySelector('[data-js="bookmark"]')
-const showAnswer = document.querySelector('[data-js="show-answer"]')
-const answer = document.querySelector('[data-js="answer"]')
+const bookmark = document.querySelector('[data-js="bookmark"]');
+const showAnswer = document.querySelector('[data-js="show-answer"]');
+const answer = document.querySelector('[data-js="answer"]');
+// const form = document.querySelector('[data-js="form"]');
 
 bookmark.addEventListener("click", (event) => {
-    bookmark.classList.toggle("question-card__item__bookmark__filled")
-})
+  bookmark.classList.toggle("question-card__item__bookmark__filled");
+});
 
 showAnswer.addEventListener("click", (event) => {
-    
-    if(answer.hidden) {
-        answer.hidden = false;
-        showAnswer.textContent = "Hide Answer"
-        return
-    }   answer.hidden = true;
-        showAnswer.textContent = "Show Answer"
-})
+  if (answer.hidden) {
+    answer.hidden = false;
+    showAnswer.textContent = "Hide Answer";
+    return;
+  }
+  answer.hidden = true;
+  showAnswer.textContent = "Show Answer";
+});
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   const formElements = event.target.elements;
+//   console.log(formElements);
+
+//   event.target.elements.question.focus();
+
+//   const formData = new FormData(event.target);
+//   const data = Object.fromEntries(formData);
+
+
+// });
