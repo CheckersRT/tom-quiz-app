cf3e78b053fd2ed0f1ca32a49657ba3704570eb9
Tom Dent <tomdent991@hotmail.com>
2023-11-14 23:28:05 +0100

    attempt at query all failed

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-const bookmark = document.querySelector('[data-js="bookmark"]');
+const bookmark = document.querySelectorAll('[data-js="bookmark"]');
 const showAnswer = document.querySelector('[data-js="show-answer"]');
 const answer = document.querySelector('[data-js="answer"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 
-bookmark.addEventListener("click", (event) => {
-  bookmark.classList.toggle("question-card__item__bookmark__filled");
+bookmark.addEventListener("click", function () {
+  for (const marks of bookmark) {
+    bookmark.classList.toggle("question-card__item__bookmark__filled");
+  }
 });
 
 let mouseStartX;
